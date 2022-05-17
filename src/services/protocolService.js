6f1b0f9e2dc9e3b80d68b02045762f128d9d63de
PoweRGbg <roumen_georgiev@yahoo.com>
2022-05-17 11:34:54 +0300

    getProtocol by patientID and update protocol functionality added

--- src/services/protocolService.js
+++ src/services/protocolService.js
@@ -24,9 +24,29 @@ export const getOne = (protocolId) => {
         .then(res => res.json())
 };
 
+// Get by patient
+// GET /data/comments?where=recipeId%3D%228f414b4f-ab39-4d36-bedb-2ad69da9c830%22
+export const getByPatient = (patientId, token) => {
+    return fetch(`${baseUrl}/protocols?where=patientId%3D%22${patientId}%22`, {
+        method: 'GET',
+        headers: {
+            'X-Authorization': token
+        }
+    }).then(res => res.json());
+};
+
 export const remove = (protocolId, token) => {
     return fetch(`${baseUrl}/protocols/${protocolId}`, {
         method: 'DELETE',
+        headers: {
+            'X-Authorization': token
+        }
+    }).then(res => res.json());
+};
+
+export const update = (protocolId, token) => {
+    return fetch(`${baseUrl}/protocols/${protocolId}`, {
+        method: 'PUT',
         headers: {
             'X-Authorization': token
         }
