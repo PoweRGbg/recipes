e9ea3ce8bb05ed55c3a3edeb191491b8ce0c598a
PoweRGbg <roumen_georgiev@yahoo.com>
2022-05-18 11:04:38 +0300

    getByProtocol function

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -24,10 +24,17 @@ export const getOne = (recipeId) => {
         .then(res => res.json())
 };
 
-// Get by patient
-// GET /data/recipes?where=recipeId%3D%228f414b4f-ab39-4d36-bedb-2ad69da9c830%22
 export const getByPatient = (patientId, token) => {
     return fetch(`${baseUrl}/recipes?where=patientId%3D%22${patientId}%22`, {
+        method: 'GET',
+        headers: {
+            'X-Authorization': token
+        }
+    }).then(res => res.json());
+};
+
+export const getByprotocol = (protocolId, token) => {
+    return fetch(`${baseUrl}/recipes?where=protocolId%3D%22${protocolId}%22`, {
         method: 'GET',
         headers: {
             'X-Authorization': token
